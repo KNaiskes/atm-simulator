bae4eef31c42de2df61df1e0690ff71739421117
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2020-11-01 11:18:04 +0200

    Add Menu class with getCustomerOption

--- src/com/knaiskes/Main.java
+++ src/com/knaiskes/Main.java
@@ -1,6 +1,7 @@
 package com.knaiskes;
 
 import com.knaiskes.fakeDB.FakeDB;
+import com.knaiskes.menu.Menu;
 import com.knaiskes.model.Customer;
 
 import java.util.HashMap;
@@ -10,10 +11,14 @@ public class Main {
 
     public static void main(String[] args) {
         FakeDB fakeDB = new FakeDB();
+        Menu menu = new Menu();
+
         Customer customer = new Customer( "Kiriakos", "Naiskes", "somemail@example.com","123", 100.43);
         Customer customer2 = new Customer( "Frodo", "Baggins", "frodo@example.com" ,"the one ring", 50.43);
 
         fakeDB.addCustomer(customer);
         fakeDB.addCustomer(customer2);
+
+        menu.getCustomerOption();
     }
 }
--- src/com/knaiskes/menu/Menu.java
+++ src/com/knaiskes/menu/Menu.java
@@ -1,2 +1,42 @@
-package com.knaiskes.menu;public class Menu {
+package com.knaiskes.menu;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Menu {
+    private String[] options = {
+            "1: Withdraw money from your account",
+            "2: Deposit money to your account",
+            "3: View your balance",
+            "4: Change your password"
+    };
+    private int selectOption;
+
+    public Menu() {}
+
+    public int getSelectOption() {
+        return selectOption;
+    }
+
+    public void setSelectOption(int sellectOption) {
+        this.selectOption = sellectOption;
+    }
+
+    public void getCustomerOption() {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Select your option by entering its number");
+        for (int i = 0; i < options.length; i++) {
+            System.out.println(options[i]);
+        }
+        System.out.print("> ");
+        try {
+            selectOption = in.nextInt();;
+            this.setSelectOption(selectOption);
+            System.out.println("Here: " + this.getSelectOption());
+        } catch (InputMismatchException e) {
+            System.out.println("Input must be an integer");
+            this.getCustomerOption();
+        }
+    }
 }
